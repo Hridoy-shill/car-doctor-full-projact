d96aa32988e0fdf356056062ec5976817514f6c7
Hridoy-shill <ridoyaaa60@gmail.com>
2023-05-11 12:41:12 +0600

    json data updated remove _id

--- src/Pages/SingUp/SingUp.jsx
+++ src/Pages/SingUp/SingUp.jsx
@@ -3,29 +3,29 @@ import img from '../../assets/images/login/login.svg'
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../Home/Provider/AuthProvider';
 
+const SingUp = () => {
 
-const LoginPage = () => {
-    const {loginUser} = useContext(AuthContext)
+    const {createNewUser} = useContext(AuthContext);
 
     const handleFromData = (event) => {
         event.preventDefault()
         const form = event.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        console.log(name, email, password);
 
-        loginUser(email, password)
-        .then(result => {
-            const loggedUser = result.user;
-            console.log(loggedUser);
-            form.reset();
+        createNewUser(email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+            form.reset()
         })
-        .catch(error =>{
+        .catch(error=>{
             console.log(error);
         })
+
     }
-
-
 
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -34,29 +34,35 @@ const LoginPage = () => {
                     <img src={img} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <h1 className="text-3xl mt-3 text-center font-bold">Login</h1>
+                    <h1 className="text-3xl mt-3 text-center font-bold">SingUp</h1>
                     <form onSubmit={handleFromData}>
                         <div className="card-body">
                             <div className="form-control">
                                 <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" placeholder="Name" name='name' className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" name='email' className="input input-bordered" />
+                                <input type="email" placeholder="email" name='email' className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <input className='btn btn-primary' type="submit" value="Login" />
+                                <input className='btn btn-primary' type="submit" value="SingUp" />
                             </div>
 
-                            <p>create an account<Link to={'/singup'} className='text-base font-bold text-orange-600 ms-3'>SingUp</Link></p>
+                            <p>already have an account<Link to={'/login'} className='text-base font-bold text-orange-600 ms-3'>Login</Link></p>
                         </div>
                     </form>
                 </div>
@@ -65,4 +71,4 @@ const LoginPage = () => {
     );
 };
 
-export default LoginPage;
+export default SingUp;
